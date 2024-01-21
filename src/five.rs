0613740903c897f447bd6124dc1561883c6f14e7
Zan Baldwin <hello@zanbaldwin.com>
2024-01-21 15:25:43 +0100

    Day Five

---
+++ src/five.rs
@@ -0,0 +1,28 @@
+use std::{cmp::min, collections::HashMap};
+
+use axum::{extract::Query, routing::post, Json, Router};
+use serde_json::Value;
+
+pub fn router() -> Router {
+    Router::new().route("/", post(names))
+}
+
+async fn names(
+    Query(params): Query<HashMap<String, usize>>,
+    Json(names): Json<Vec<String>>,
+) -> Json<Value> {
+    let offset = params.get("offset").unwrap_or(&0).clone();
+
+    let names = match params.get("limit") {
+        Some(limit) => names[offset..min(names.len(), offset + limit)].to_vec(),
+        None => names[offset..].to_vec(),
+    };
+
+    let names = if let Some(split) = params.get("split").copied() {
+        serde_json::to_value(names.chunks(split).collect::<Vec<_>>())
+    } else {
+        serde_json::to_value(names)
+    };
+
+    Json(names.unwrap())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 
+mod five;
 mod four;
 mod minus_one;
 mod one;
@@ -9,7 +10,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .merge(minus_one::router())
         .nest("/1", one::router())
-        .nest("/4", four::router());
+        .nest("/4", four::router())
+        .nest("/5", five::router());
 
     Ok(router.into())
 }
