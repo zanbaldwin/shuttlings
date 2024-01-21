445a17dc33d32f08865fb18cccf473d946486617
Zan Baldwin <hello@zanbaldwin.com>
2024-01-21 13:07:02 +0100

    $ cargo shuttle init --template axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(hello_world));
+
+    Ok(router.into())
 }
